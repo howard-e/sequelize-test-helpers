617335d7f04cdb9451d5d6eda84c3f6ccafd669b
Howard Edwards <howarde.edwards@gmail.com>
2024-08-17 10:20:42 -0400

    Update error messages

--- src/checks/checkHookDefined.js
+++ src/checks/checkHookDefined.js
@@ -1,8 +1,9 @@
 const checkHookDefined = instance => hookName => {
   if (typeof instance.hooks[hookName] !== 'function') {
-    throw new Error()
+    throw new Error(
+      `Expected 'instance.hooks[hookName]' to be a function. Received ${typeof instance.hooks[hookName]} instead.`
+    )
   }
-
   return instance.hooks[hookName]
 }
 
--- src/checks/checkModelName.js
+++ src/checks/checkModelName.js
@@ -1,8 +1,9 @@
 const checkModelName = model => modelName => {
   if (model.modelName !== modelName) {
-    throw new Error()
+    throw new Error(
+      `Expected 'model.modelName' to be the same as 'modelName'. Received ${model.modelName} and ${modelName} instead.`
+    )
   }
-
   return modelName
 }
 
